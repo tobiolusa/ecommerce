cd5c2b1a544735c8e3d74929265c00b1800eaa39
tobiolusa <olusamiracle@gmail.com>
2024-01-19 04:01:36 +0100

    Product display on homepage

--- storeapp/views.py
+++ storeapp/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from .models import Category, Product
 
 def store(request):
-    return render(request, 'storeapp/store.html')
+    all_products = Product.objects.all()
+    context = {'all_products': all_products}
+    return render(request, 'storeapp/store.html', context)
+
+def categories(request):
+    all_categories = Category.objects.all()
+    return {'all_categories': all_categories}
